6af44739b7249e19764e4bd1325382b939ab9bdb
dre <alipson@pm.me>
2024-05-02 18:28:08 -0700

    fixed the decrypt and cracked the code

--- encrypt.py
+++ encrypt.py
@@ -1,8 +1,10 @@
 import numpy as np
 from numpy.typing import NDArray
 
-A = np.array([[9, 3], [4, 5]])
-A_invs = np.array([[23, 7], [18, 5]])
+# results from project
+# TODO: these are incorrect!
+A_invs = np.array([[9, 3], [4, 5]])
+A = np.array([[23, 7], [18, 5]])
 
 
 def matrix_from_text(s: str) -> NDArray[np.float64]:
@@ -16,8 +18,8 @@ def matrix_from_text(s: str) -> NDArray[np.float64]:
     # group indices into pairs in a 2-row matrix
     paired_indices = [indices[i:i+2] for i in range(0, len(indices), 2)]
 
-    # convert list matrix into an np array 
-    return np.array()
+    # convert list matrix into a 2-row np array
+    return np.array(paired_indices).T
 
 
 def text_from_matrix(arr: NDArray[np.float64]) -> str:
@@ -38,12 +40,26 @@ def decrypt_matrix(M: NDArray[np.float64]) -> NDArray[np.float64]:
     return np.dot(A_invs, M)
 
 
+# bind 26 for alphabet modulo
+mod_26 = lambda arr: matrix_modulo(arr, 26)
+
+ciphertext = "SONAFQCHMWPTVEVY"
+E = mod_26(matrix_from_text(ciphertext))
+
+print(mod_26(encrypt_matrix(E)))
+
+print(text_from_matrix(mod_26(encrypt_matrix(E))))
+
 M = np.array(
     [[512, 299, 227, 95, 683, 478, 511, 651], [394, 234, 170, 71, 524, 365, 398, 49]]
 )
 
-# bind 26 for alphabet modulo
-mod_26 = lambda arr: matrix_modulo(arr, 26)
-
 print(mod_26(M))
-print(text_from_matrix(mod_26(M)))
+# print(text_from_matrix(mod_26(M)))
+
+
+# test inverse
+message = "THISISATEST"
+E = mod_26(encrypt_matrix(matrix_from_text(message)))
+print(mod_26(decrypt_matrix(E)))
+print(text_from_matrix(mod_26(decrypt_matrix(E))))
